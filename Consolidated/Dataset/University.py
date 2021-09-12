88b96825f22e5b499ea1f4ca99f0dda04e0824e4
Aina Gbolahan <86109294+Ainagbolahan@users.noreply.github.com>
2021-09-12 23:48:48 +0100

    Added Streamlit app

---
+++ Consolidated/Dataset/University.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pickle
+import streamlit as st
+import numpy as np
+
+# loading the trained model
+pickle_in = open('classifier.pkl', 'rb') 
+classifier = pickle.load(pickle_in)
+
+@st.cache()
+
+# defining the function which will make the prediction using the data which the user inputs 
+def prediction(No_of_FTE_Students, No_of_students_per_staff,International_Students, Teaching, Research, Citations,
+       Industry_Income, International_Outlook):
+
+
+# Making predictions 
+    prediction = classifier.predict(np.array( [[No_of_FTE_Students, No_of_students_per_staff,International_Students, Teaching, Research, Citations, Industry_Income, International_Outlook]]))
+    if prediction <= 0:
+        pred = 'Badly Ranked'
+    else:
+        pred = 'Good Ranking'
+    return pred
+
+
+#this is the main function in which we define our webpage  
+def main(): 
+    
+#front end elements of the web page 
+
+    html_temp = """ 
+    <div style ="background-color:yellow;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Streamlit University Ranking ML App</h1> 
+    </div> 
+    """
+
+#display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html = True) 
+
+#following lines create boxes in which user can enter data required to make prediction 
+    No_of_FTE_Students = st.number_input("No_of_FTE_Students")
+    No_of_students_per_staff = st.number_input("No_of_students_per_staff")
+    International_Students = st.number_input('International_Students')
+    Teaching = st.number_input("Teaching")
+    Research = st.number_input("Research")
+    Citations = st.number_input("Citations")
+    Industry_Income = st.number_input("Industry_Income")
+    International_Outlook = st.number_input("International_Outlook")  
+     
+    result =""
+
+#when 'Predict' is clicked, make the prediction and store it 
+    if st.button("Predict"): 
+        result = prediction(No_of_FTE_Students, No_of_students_per_staff,
+           International_Students, Teaching, Research, Citations,
+           Industry_Income, International_Outlook) 
+        st.success('Your Rank Score is {}'.format(result))
+
+
+if __name__=='__main__': 
+    main()
+
+
+# In[ ]:
+
+
+
+
