ee5bbc35a27f59eb4bcf313406c4f709cd39ff59
Aina Gbolahan <86109294+Ainagbolahan@users.noreply.github.com>
2021-09-18 09:04:33 +0100

    Updated App

--- Consolidated/Dataset/Stream_lit_app.py
+++ Consolidated/Dataset/Stream_lit_app.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[7]:
 
 
 #np.array( [[scaled_features]].values.reshape(-1, 1)
 
 
-# In[3]:
+# In[27]:
 
 
 import pickle
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 # loading the trained model
 pickle_in = open('svr_model.pkl', 'rb') 
@@ -21,14 +22,20 @@ classifier = pickle.load(pickle_in)
 @st.cache()
 
 # defining the function which will make the prediction using the data which the user inputs 
-def prediction(No_of_FTE_Students, No_of_students_per_staff,International_Students, Teaching, Research, Citations,
-       Industry_Income, International_Outlook,Year):
-
-
+def prediction(University_name,No_of_FTE_Students,No_of_students_per_staff,International_Students,Teaching,Research,Citations,Industry_Income,International_Outlook,Year,Country):
+    University_name = [" "]
+    Country = [" "]
+    from sklearn.preprocessing import LabelEncoder
+    le = LabelEncoder()
+    University_name = le.fit_transform(University_name)
+    Country = le.fit_transform(Country)
+    
+ 
 
 # Making predictions 
-    prediction = classifier.predict(np.array([[No_of_FTE_Students, No_of_students_per_staff,International_Students, Teaching, Research, Citations,
-       Industry_Income, International_Outlook,Year]]) )
+    prediction = classifier.predict([[University_name,No_of_FTE_Students,No_of_students_per_staff,International_Students,
+               Teaching,Research,Citations,Industry_Income,International_Outlook,Year,Country]])
+
     if prediction <= 0:
         pred = 'Confirm Accuracy of your data'
     else:
@@ -60,15 +67,16 @@ def main():
     Industry_Income = st.number_input("Industry_Income")
     International_Outlook = st.number_input("International_Outlook")  
     Year = st.number_input("Year") 
+    University_name = st.text_input("University Name")
+    Country = st.text_input("Country Name ")
      
     result =""
 
 #when 'Predict' is clicked, make the prediction and store it 
     if st.button("Predict"): 
-        result = prediction(No_of_FTE_Students, No_of_students_per_staff,
-           International_Students, Teaching, Research, Citations,
-           Industry_Income, International_Outlook,Year) 
-        st.success('Your Rank Score is {}'.format(result))
+        result = prediction(University_name,No_of_FTE_Students,No_of_students_per_staff,International_Students,Teaching,Research,Citations,Industry_Income,International_Outlook,Year,Country)
+
+        st.success('Your school ranks {}'.format(result))
 
 
 if __name__=='__main__': 
@@ -79,6 +87,24 @@ if __name__=='__main__':
 
 
 
+
+
+# In[25]:
+
+
+University_name = ["Uniben"]
+Country = ["Niger"]
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+University_name = le.fit_transform(University_name)
+Country = le.fit_transform(Country)
+
+
+# In[26]:
+
+
+Country
 
 
 # In[ ]:
